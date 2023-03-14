7d2c6180233d7edc3a40eb2dea385ffd406a814a
Phazze <nekrashevich.gleb@gmail.com>
2023-03-14 20:25:39 +0300

    New sql zapros

--- main.py
+++ main.py
@@ -12,16 +12,16 @@ try:
     )
     print("all good")
     print("######" * 10)
-
+    login = str(input())
+    user_password = str(input())
     try:
         with mydb.cursor() as cursor:
-            cursor.execute("SHOW DATABASES;")
-            print(cursor.fetchall())
+            sql_zapros = f"SELECT first_name, Last_name, age, informachion from users, connect where connect.login = '{login}' and connect.password = '{user_password}' and connect.id = users.fk_id;"
+            cursor.execute(sql_zapros)
+            vivod = cursor.fetchall()
+            print(vivod)
     finally:
         mydb.close()
-
-
-
 except Exception as ex:
     print("Connection failed....")
     print(ex)
