e7425dc29e171681c2add0e73e2e62f60f7e4e63
Phazze <nekrashevich.gleb@gmail.com>
2023-03-15 22:38:54 +0300

    Class MySQL

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 import pymysql
 from auth import user, password, database, host
 
-
-try:
-    mydb = pymysql.connect(
-        host=host,
-        password=password,
-        port=3306,
-        user=user,
-        database=database
-    )
-    print("all good")
-    print("######" * 10)
-    login = str(input())
-    user_password = str(input())
+class MySQL():
     try:
-        with mydb.cursor() as cursor:
-            sql_zapros = f"SELECT first_name, Last_name, age, informachion from users, connect where connect.login = '{login}' and connect.password = '{user_password}' and connect.id = users.fk_id;"
-            cursor.execute(sql_zapros)
-            vivod = cursor.fetchall()
-            print(vivod)
-    finally:
-        mydb.close()
-except Exception as ex:
-    print("Connection failed....")
-    print(ex)
+        mydb = pymysql.connect(
+            host=host,
+            password=password,
+            port=3306,
+            user=user,
+            database=database
+        )
+        print("all good")
+        print("######" * 10)
+        login = str(input())
+        user_password = str(input())
+        try:
+            with mydb.cursor() as cursor:
+                sql_zapros = f"SELECT first_name, Last_name, age, informachion from users, connect where connect.login = '{login}' and connect.password = '{user_password}' and connect.id = users.fk_id;"
+                cursor.execute(sql_zapros)
+                vivod = cursor.fetchall()
+                print(vivod)
+        finally:
+            mydb.close()
+    except Exception as ex:
+        print("Connection failed....")
+        print(ex)
 
